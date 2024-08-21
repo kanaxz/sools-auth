33dd7ee62e9d0370cbbb79e1f0437026e08f1fac
kanaxz <kanaxstratz@gmail.com>
2024-08-21 12:05:32 +0200

    1.9.0

--- User.js
+++ User.js
@@ -2,26 +2,10 @@ const { Model, String } = require('sools-modeling/types')
 const mixer = require('sools-core/mixer')
 const Pageable = require('sools-modeling/mixins/Pageable')
 const Wikiable = require('sools-wiki/mixins/Wikiable')
-const ControllerError = require('sools-modeling/controlling/ControllerError')
 const setup = require('./setup')
 
-const isSelfOrAdmin = async (context, user) => {
-  if (!context.user) {
-    throw new ControllerError('User is not logged in')
-  }
-  if (!context.user.equals(user) && !await context.user.is(context, 'admin')) {
-    throw new ControllerError(`User doesn't have sufficient rights`)
-  }
-  
-}
-
 module.exports = class User extends mixer.extends(Model, [Pageable, Wikiable, ...setup.user]) {
-  async is(context, name) {
-    await this.groups.load(context)
-    return this.groups.some((group) => group.name === name)
-  }
-
-  toString(){
+  toString() {
     return this.username
   }
 }
@@ -37,6 +21,10 @@ module.exports = class User extends mixer.extends(Model, [Pageable, Wikiable, ..
     username: {
       properties: ['username'],
       unique: true,
+    },
+    email: {
+      properties: ['email'],
+      unique: true,
     }
   })
   .properties({
@@ -46,25 +34,10 @@ module.exports = class User extends mixer.extends(Model, [Pageable, Wikiable, ..
         required: true,
       }
     },
-    password: {
+    email: {
       type: String,
       state: {
-        required: false,
+        required: true,
       }
     },
-  })
-  .controllers({
-    create: {
-      async check(context) {
-        if (!context.signup && !context.setup && (!context.user || !await context.user.is(context, 'admin'))) {
-          throw new ControllerError(`You cannot create an user`)
-        }
-      },
-    },
-    update: {
-      check: isSelfOrAdmin,
-    },
-    delete: {
-      check: isSelfOrAdmin
-    }
   })
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { HasMany, OwnMany } = require('sools-modeling/types')
+const { HasMany, OwnMany, } = require('sools-modeling/types')
 const Group = require('./Group')
 const User = require('./User')
+const Identity = require('./Identity')
 
 Group.properties({
   users: {
@@ -9,9 +10,18 @@ Group.properties({
   }
 })
 
+Identity.properties({
+  user: {
+    type: User,
+  }
+})
+
 User.properties({
   groups: {
     type: OwnMany.of(Group),
+  },
+  identities: {
+    type: HasMany.of(Identity)
   }
 })
 
